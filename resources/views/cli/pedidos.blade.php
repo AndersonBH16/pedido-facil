6b4c42885a8b156ecc6db72256c5cb75b45a35e7
AndersonBH16 <andi_1516@hotmail.com>
2020-12-02 22:55:46 -0500

    Primeras configuraciones

--- resources/views/cli/pedidos.blade.php
+++ resources/views/cli/pedidos.blade.php
@@ -1,1 +1,17 @@
-<button class="btn btn-success">Hola</button>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+                This is some text within a card body.
+            </div>
+        </div>
+    </div>
+
+    {{--@include('proformas.modal.paquete-lista-cotizar')--}}
+
+@stop
+@section('js')
+    {{--<script src="{{ asset('pc-scripts/paquete.js') }}"></script>--}}
+@stop
